60705168d13528cd50c10102e5768c986e536b8c
flash <developsoon@gmail.com>
2023-04-05 16:14:38 +0900

    apply .env setting for rpc-url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
 import { getPrivateKey } from "blockchain-key-manager";
 import { HardhatUserConfig } from "hardhat/types";
 
@@ -17,13 +18,14 @@ const config: HardhatUserConfig = {
       gasPrice: GAS_PRICE,
       hardfork: EVM_VERSION,
       forking: {
-        url: "https://api.baobab.klaytn.net:8651/",
-        blockNumber: 128, // klaytn 은 128블록마다 최신 4개의 블록에 대해서 state 정보를 저장 -> 임의의 노드에서 정보를 저장하려면 노드를 archive node로 동작시켜야 함
+        url: process.env.PUBLIC_RPC_URL!!,
+        // klaytn 은 128블록마다 최신 4개의 블록에 대해서 state 정보를 저장 -> 임의의 노드에서 정보를 저장하려면 노드를 archive node로 동작시켜야 함
         // https://forum.klaytn.foundation/t/balance/235
+        blockNumber: 128,
       },
     },
     baobab: {
-      url: "https://baobab.ken.stick.us",
+      url: process.env.BAOBAB_RPC_URL!!,
       gas: GAS_LIMIT,
       gasPrice: GAS_PRICE,
       hardfork: EVM_VERSION,
